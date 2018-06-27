4237da1797350c7f0f0bb4c5f083a54880013269
gwapesofwath <gwapesofwath@gmail.com>
2018-06-26 21:33:01 -0500

    initial commit

---
+++ javascript/app.js
@@ -0,0 +1,58 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+var app = express();
+
+var PORT = process.env.PORT || 3000;
+
+var guestCount = 0;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+//create the server to listen
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+
+//create an empty array to hold new restraurant guests
+  var reservations = [];
+
+  var waitlist = [];
+
+//Create routes
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/reservations", function(req, res) {
+    res.send(reservations)
+    res.sendFile(path.join(__dirname, "reservations.html"));
+});
+
+app.get("/tables", function(req, res) {
+    res.sendFile(path.join(__dirname, "tables.html"));
+});
+
+
+//Updating new reservation
+
+app.post("/api/reservations", function (req, res) {
+    var newGuest = req.body;
+    console.log(newGuest);
+
+    guestCount++;
+    if (guestCount > 5) {
+        waitlist.push(newGuest);
+    }
+    else {
+        reservations.push(newGuest);
+    }
+    res.json(newGuest);
+});
+
+  
